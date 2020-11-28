5fe19f7e7af5652d1b1af9ee2a4b98a91b7a6620
loveisgala <gabriela.gk@hotmail.com>
2020-11-28 15:35:14 -0300

    capturing data

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
-const fetchData = async() => {
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData()
+})
 
+const fetchData = async() => {
+    try {
+        const res = await fetch('/api.json')
+        const data = await res.json()
+        console.log(data)
+    } catch (error) {
+        console.log(error)
+    }
 }
