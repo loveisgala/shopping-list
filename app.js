b25098ead3c3bac0242b333549e5d54a6da29e5b
loveisgala <gabriela.gk@hotmail.com>
2020-11-28 15:57:16 -0300

    add const pintarCards

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const items = document.getElementById('items')
 const templateCard = document.getElementById('template-card').content
+const fragment = document.createDocumentFragment()
 
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
@@ -17,5 +18,14 @@ const fetchData = async() => {
 }
 
 const pintarCards = data => {
-    console.log(data)
+
+    data.forEach(producto => {
+        templateCard.querySelector('h5').textContent = producto.title
+        templateCard.querySelector('p').textContent = producto.precio
+        templateCard.querySelector('img').setAttribute("src", producto.thumbnailUrl)
+        templateCard.querySelector('.btn-dark').dataset.id = producto.id
+        const clone = templateCard.cloneNode(true)
+        fragment.appendChild(clone)
+    })
+    items.appendChild(fragment)
 }
