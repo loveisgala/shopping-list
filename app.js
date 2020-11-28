9bba8ecbac13a883545722bc5a76b84f76029251
loveisgala <gabriela.gk@hotmail.com>
2020-11-28 15:42:03 -0300

    create const items and templateCard

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const items = document.getElementById('items')
+const templateCard = document.getElementById('template-card').content
+
 document.addEventListener('DOMContentLoaded', () => {
     fetchData()
 })
@@ -6,8 +9,13 @@ const fetchData = async() => {
     try {
         const res = await fetch('/api.json')
         const data = await res.json()
-        console.log(data)
+            //console.log(data)
+        pintarCards(data)
     } catch (error) {
         console.log(error)
     }
+}
+
+const pintarCards = data => {
+    console.log(data)
 }
